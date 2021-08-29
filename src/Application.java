d8b2a7296af29e414496e58ab5662867f91a3e03
tadeucosta <antoniotadeucosta@gmail.com>
2021-08-29 00:08:32 -0300

    primeiro commit

---
+++ src/Application.java
@@ -0,0 +1,10 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá mundo java no Mac Book Air M1");
+
+	}
+
+}
